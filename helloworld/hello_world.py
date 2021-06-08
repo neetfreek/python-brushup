6b1b00b75e7c9c00b6c6110365ac357b2e80b5a7
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-06-08 18:03:24 +0300

    Add time-based greeting to hello world, comments and docstrings

--- helloworld/hello_world.py
+++ helloworld/hello_world.py
@@ -2,5 +2,10 @@ from helloworld import time_message
 
 
 def hello_world():
-    print("Hello, world!")
-    print(time_message.get_time())
+    """Print hello world with greeting based on current time."""
+    _print_time_based_hello_world()
+
+
+# Print good morning/afternoon/evening and hello world.
+def _print_time_based_hello_world():
+    print(f"{time_message.get_time_based_greeting()}, and hello, world!")
--- helloworld/time_message.py
+++ helloworld/time_message.py
@@ -1,5 +1,17 @@
-import datetime
+from _datetime import datetime
 
 
-def get_time():
-    return datetime.datetime.now()
+def get_time_based_greeting():
+    """Return appropriate greeting for the current time for greetings."""
+    return _get_message_for_current_time()
+
+
+# Return a suitable message based on the current time.
+def _get_message_for_current_time():
+    hour_now = datetime.now().hour
+    if hour_now < 12:
+        return "Good morning"
+    elif hour_now < 18:
+        return "Good afternoon"
+    else:
+        return "Good evening"
