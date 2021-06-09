1ddf9c2cbe361d0f0a4a90ecd94de82db5763c99
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-06-09 19:09:16 +0300

    Create fizzbuzz module

---
+++ fizzbuzz/fizz_buzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz():
+    """Fizz buzz exercise"""
+    for i in range(1, 101):
+        if i % 3 == 0:
+            if i % 15 == 0:
+                print("fizzbuzz")
+            else:
+                print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from helloworld import hello_world
+from fizzbuzz import fizz_buzz
 
 hello_world.hello_world()
+fizz_buzz.fizz_buzz()
