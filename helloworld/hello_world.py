71de4f1838389d5337afd7e33499158dd1ad1873
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-06-11 23:19:55 +0300

    Refactor hello_world

--- helloworld/hello_world.py
+++ helloworld/hello_world.py
@@ -1,4 +1,4 @@
-from helloworld import time_message
+from _datetime import datetime
 
 
 def hello_world():
@@ -8,4 +8,18 @@ def hello_world():
 
 # Print good morning/afternoon/evening and hello world.
 def _print_time_based_hello_world():
-    print(f"{time_message.get_time_based_greeting()}, and hello, world!")
+    print(f"{_get_message_for_current_time()}, and hello, world!")
+
+
+# Return a suitable message based on the current time.
+def _get_message_for_current_time():
+    hour_now = datetime.now().hour
+    if hour_now < 12:
+        return "Good morning"
+    elif hour_now < 18:
+        return "Good afternoon"
+    else:
+        return "Good evening"
+
+
+hello_world()
