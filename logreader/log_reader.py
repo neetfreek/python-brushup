0512f654c67b57b22975fe7a40b5c81cc1eec0f7
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-06-20 14:42:12 +0300

    Remove non-exsitent logfiles from list of logs to consider

--- logreader/log_reader.py
+++ logreader/log_reader.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python
 """Read selected log files, copying those with failures and errors into
-./logs{log file name}.
-Lines of interest are written to ./logs{log file name}_issues.log
+DIR_PROCESSED_LOGS/{log file name}.
+Lines of interest are written to DIR_PROCESSED_LOGS/{log file name}_issues.log
 """
 
 from pathlib import Path
 
-DIR_READ_LOGS = Path("./logs")
-PATH_READ_LOGS = Path.cwd() / DIR_READ_LOGS
-
-log_dir = Path('/var/log')
-log_files = [Path("boot.log"), "messages.log", "auth.log", "daemon.log",
-             "kern.log"]
+DIR_PROCESSED_LOGS = Path("./logs")
+DIR_LOGS = Path('/var/log')
+LOG_FILES_WANTED = [Path("boot.log"), Path("messages"), Path("auth.log"),
+                    Path("daemon.log"), Path("kern.log"), Path("some.nonsense")]
+PATH_READ_LOGS = Path.cwd() / DIR_PROCESSED_LOGS
 
 
 def _create_logs_dir():
     # Create dir for read logs if it doesn't exist.
     if not PATH_READ_LOGS.is_dir():
-        print(f"PATH_READ_LOGS does not exist")
         Path(PATH_READ_LOGS).mkdir(parents=True, exist_ok=True)
 
 
-print(Path.cwd())
-_create_logs_dir()
+def _remove_nonexistent_files(log_dir, log_files):
+    # Remove non-existent files from logs to consider
+    log_files_existing = log_files.copy()
+    for log_file in log_files:
+        if not Path(log_dir / log_file).is_file():
+            print(f"Log file {log_file} does not exist. Removing from log "
+                  f"reading")
+            log_files_existing.remove(log_file)
+    return log_files_existing
+
+
+def main():
+    global LOG_FILES_WANTED
+    print(Path.cwd())
+    _create_logs_dir()
+    print(LOG_FILES_WANTED[1].name)
+    LOG_FILES_WANTED = _remove_nonexistent_files(DIR_LOGS, LOG_FILES_WANTED)
+    print(LOG_FILES_WANTED)
+
+
+main()
