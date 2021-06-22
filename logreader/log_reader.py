eab36c613ec0669db8ca344fe5399c9fd0c0d08a
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-06-22 17:57:04 +0300

    Refactor, reorganise

--- logreader/log_reader.py
+++ logreader/log_reader.py
@@ -7,7 +7,6 @@ Log files with issues have issues extracted and written to
 """
 
 # TODO: Add line numbers for issue and copy files
-# TODO: Replace multiple instances of issues with counts instead
 # TODO: Write different logs to different directories in logs
 # TODO: Add flags for:
 #   -d directory containing log files
@@ -37,17 +36,6 @@ def _create_logs_dir():
         Path(PATH_READ_LOGS).mkdir(parents=True, exist_ok=True)
 
 
-def _remove_nonexistent_files(log_dir, log_files):
-    # Return list without log files which cannot be found
-    log_files_existing = log_files.copy()
-    for log_file in log_files:
-        if not Path(log_dir / log_file).is_file():
-            print(f"Log file {log_file} does not exist. Removing from log "
-                  f"reading")
-            log_files_existing.remove(log_file)
-    return log_files_existing
-
-
 def _set_abs_log_file_paths(log_files):
     # Return updated list with absolute file paths for log files
     log_files_abs = []
@@ -59,18 +47,23 @@ def _set_abs_log_file_paths(log_files):
     return log_files_abs
 
 
-def _check_logfiles_for_issues(all_logfiles):
+def _remove_nonexistent_files(files_logs_paths):
+    # Return list without log files which cannot be found
+    for log_file in files_logs_paths:
+        if not log_file.is_file():
+            print(f"Log file {log_file} does not exist. Removing from log "
+                  f"reading")
+            files_logs_paths.remove(log_file)
+    return files_logs_paths
+
+
+def _iterate_check_each_file(all_logfiles):
     # Routine to call functionality to check all logfiles for issues
     for file in all_logfiles:
-        _call_check_logs_if_have_permission(file)
-
-
-def _call_check_logs_if_have_permission(file_to_read):
-    # Call to check logs if user has permission, else print warning
-    try:
-        _check_logs_for_issues(file_to_read)
-    except PermissionError:
-        print(f"You require administrator privileges to access {file_to_read}")
+        try:
+            _check_logs_for_issues(file)
+        except PermissionError:
+            print(f"You require administrator privileges to access {file}")
 
 
 def _check_logs_for_issues(file_to_read):
@@ -88,7 +81,7 @@ def _check_logs_for_issues(file_to_read):
                         logs_issues_file = \
                             _get_logs_issues_filename(logs_copy_file)
                         logs_copy_made = logs_copy_file is not None
-                        print(f"Issues found in {file_to_read.name}:\n"
+                        print(f"\nIssues found in {file_to_read.name}:\n"
                               f"See {logs_copy_file} directory for logs copy\n"
                               f"See {logs_issues_file} directory for issues")
                     if col not in issues_found:
@@ -100,12 +93,14 @@ def _check_logs_for_issues(file_to_read):
         write_log_file_issues(logs_issues_file, issues_found)
 
 
-def write_log_file_issues(logs_issues_file, issues_found):
-    with open(logs_issues_file, "a") as file:
-        for issue in issues_found:
-            file.writelines(f"ISSUE (appeared {issues_found[issue]} times):\n"
-                            f"  {issue}\n\n")
-    file.close()
+def _write_log_file_copy(file_to_copy):
+    # Create copy of passed log file
+    time_stamp = _get_formatted_timestamp()
+    filename = file_to_copy.name.replace(file_to_copy.suffix, "")
+    dest_filename = f"{filename}_{time_stamp}{file_to_copy.suffix}"
+    Path(DIR_PROCESSED_LOGS / dest_filename) \
+        .write_text(Path(file_to_copy).read_text())
+    return Path(DIR_PROCESSED_LOGS / dest_filename)
 
 
 def _get_formatted_timestamp():
@@ -113,28 +108,6 @@ def _get_formatted_timestamp():
     time_stamp = datetime.now().isoformat()
     time_stamp = time_stamp.replace(":", "")
     return time_stamp[:time_stamp.index(".")]
-
-
-def _get_filenames(file_to_create_for, time_stamp):
-    # Return name for log copy, extracted log line files
-    filename = file_to_create_for.name.replace(file_to_create_for.suffix, "")
-    extracted_log_filename = \
-        f"{filename}_extracts_{time_stamp}{file_to_create_for.suffix}"
-    copy_log_filename = \
-        f"{filename}_{time_stamp}{file_to_create_for.suffix}"
-
-    return copy_log_filename, extracted_log_filename
-
-
-def _write_log_file_copy(file_to_copy):
-    # Create copy of passed log file
-    time_stamp = _get_formatted_timestamp()
-    filename = file_to_copy.name.replace(file_to_copy.suffix, "")
-    dest_filename = f"{filename}_{time_stamp}{file_to_copy.suffix}"
-    print(f"CREATE FILE AT {dest_filename}")
-    Path(DIR_PROCESSED_LOGS / dest_filename) \
-        .write_text(Path(file_to_copy).read_text())
-    return Path(DIR_PROCESSED_LOGS / dest_filename)
 
 
 def _get_logs_issues_filename(log_file_copy):
@@ -146,11 +119,11 @@ def _get_logs_issues_filename(log_file_copy):
     return issues_path
 
 
-def _write_log_line(file_to_write, line_number, line):
-    # Write log issue found, first line number, number times found to file
-    with open(file_to_write, "a") as file:
-        file.writelines(f"ISSUE: {line}\nFOUND FIRST ON LINE {line_number}"
-                        f"\nFOUND n TIMES\n\n")
+def write_log_file_issues(logs_issues_file, issues_found):
+    with open(logs_issues_file, "a") as file:
+        for issue in issues_found:
+            file.writelines(f"ISSUE (appeared {issues_found[issue]} times):\n"
+                            f"  {issue}\n\n")
     file.close()
 
 
@@ -165,7 +138,8 @@ def main():
     global FILES_LOGS_PATHS
     _create_logs_dir()
     FILES_LOGS_PATHS = _set_abs_log_file_paths(NAMES_FILES_LOGS)
-    _check_logfiles_for_issues(FILES_LOGS_PATHS)
+    FILES_LOGS_PATHS = _remove_nonexistent_files(FILES_LOGS_PATHS)
+    _iterate_check_each_file(FILES_LOGS_PATHS)
     _delete_logs_dir_if_empty()
 
 
