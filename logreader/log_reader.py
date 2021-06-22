ec41ce0a6834836109ecf96acd420f1b1d02b90f
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-06-22 16:42:37 +0300

    Refactor log line reading, add line numbers

--- logreader/log_reader.py
+++ logreader/log_reader.py
@@ -6,11 +6,16 @@ Log files with issues have issues extracted and written to
     DIR_PROCESSED_LOGS/{log file name}{timestamp}_issues.log
 """
 
-# TODO: Add error handling for IO operations
-# TODO: Replace multiple instances of issues with counts instead
 # TODO: Add line numbers for issue and copy files
-# TODO: Place each log file's copy, issues files in separate dir
-
+# TODO: Replace multiple instances of issues with counts instead
+# TODO: Write different logs to different directories in logs
+# TODO: Add flags for:
+#   -d directory containing log files
+#   -f file containing logs
+#   -k keywords (list strings?)
+#   -o output destination
+# TODO: Add docs for flags
+import os
 from pathlib import Path
 from datetime import datetime
 import re
@@ -33,7 +38,7 @@ def _create_logs_dir():
 
 
 def _remove_nonexistent_files(log_dir, log_files):
-    # Return updated list with non-existent log files removed
+    # Return list without log files which cannot be found
     log_files_existing = log_files.copy()
     for log_file in log_files:
         if not Path(log_dir / log_file).is_file():
@@ -57,30 +62,42 @@ def _set_abs_log_file_paths(log_files):
 def _check_logfiles_for_issues(all_logfiles):
     # Routine to call functionality to check all logfiles for issues
     for file in all_logfiles:
-        _check_logfile_for_issues(file)
+        _call_check_logs_if_have_permission(file)
 
 
-# TODO: Add error-handling
-def _check_logfile_for_issues(file_to_read):
+def _call_check_logs_if_have_permission(file_to_read):
+    # Call to check logs if user has permission, else print warning
+    try:
+        _check_logs_for_issues(file_to_read)
+    except PermissionError:
+        print(f"You require administrator privileges to access {file_to_read}")
+
+
+def _check_logs_for_issues(file_to_read):
     # Find lines in log file matching issue keywords
-    logs_copy_made = False
-    logs_issues_file = ""
-    regex = re.compile(r"error|failed|warning", re.IGNORECASE)
-
     with open(file_to_read, "r") as file:
-        for line in file:
+        count_issue_found = 0
+        issues_found = []
+        logs_copy_made = False
+        regex = re.compile(r"error|failed|warning", re.IGNORECASE)
+
+        for i, line in enumerate(file):
             cols = [col.strip() for col in line.split(":") if col]
             for col in cols:
                 if regex.search(col) is not None:
                     if not logs_copy_made:
-                        logs_copy_file = _create_log_file_copy(file_to_read)
-                        logs_issues_file = _get_logs_issues_filename(logs_copy_file)
+                        logs_copy_file = _write_log_file_copy(file_to_read)
+                        logs_issues_file = \
+                            _get_logs_issues_filename(logs_copy_file)
                         logs_copy_made = logs_copy_file is not None
                         print(f"Issues found in {file_to_read.name}:\n"
                               f"See {logs_copy_file} directory for logs copy\n"
                               f"See {logs_issues_file} directory for issues")
-                    _write_line_to_file(logs_issues_file, col)
-                    # print(f"write \n{col}\nto\n{logs_issues_file}")
+                        if col not in issues_found:
+                            issues_found.append(col)
+                            print(f"Adding {col}")
+                            _write_log_line(logs_issues_file, i, col)
+                            count_issue_found += 1
     file.close()
 
 
@@ -102,14 +119,14 @@ def _get_filenames(file_to_create_for, time_stamp):
     return copy_log_filename, extracted_log_filename
 
 
-# TODO: Include error handling for file-creation
-def _create_log_file_copy(file_to_copy):
+def _write_log_file_copy(file_to_copy):
     # Create copy of passed log file
     time_stamp = _get_formatted_timestamp()
     filename = file_to_copy.name.replace(file_to_copy.suffix, "")
     dest_filename = f"{filename}_{time_stamp}{file_to_copy.suffix}"
     print(f"CREATE FILE AT {dest_filename}")
-    Path(DIR_PROCESSED_LOGS / dest_filename).write_text(Path(file_to_copy).read_text())
+    Path(DIR_PROCESSED_LOGS / dest_filename) \
+        .write_text(Path(file_to_copy).read_text())
     return Path(DIR_PROCESSED_LOGS / dest_filename)
 
 
@@ -122,19 +139,27 @@ def _get_logs_issues_filename(log_file_copy):
     return issues_path
 
 
-def _write_line_to_file(file_to_write, line):
-    # Write lines to specified file
+def _write_log_line(file_to_write, line_number, line):
+    # Write log issue found, first line number, number times found to file
     with open(file_to_write, "a") as file:
-        file.writelines(f"{line}\n")
+        file.writelines(f"ISSUE: {line}\nFOUND FIRST ON LINE {line_number}"
+                        f"\nFOUND n TIMES\n\n")
     file.close()
 
 
+def _delete_logs_dir_if_empty():
+    for _, _, files in os.walk(DIR_PROCESSED_LOGS):
+        if not files:
+            DIR_PROCESSED_LOGS.rmdir()
+
+
 def main():
-    """"Main routine to handle all log reading, writing functionality"""
+    """Routine responsible for handling log copying, issue extraction"""
     global FILES_LOGS_PATHS
     _create_logs_dir()
     FILES_LOGS_PATHS = _set_abs_log_file_paths(NAMES_FILES_LOGS)
     _check_logfiles_for_issues(FILES_LOGS_PATHS)
+    _delete_logs_dir_if_empty()
 
 
 main()
